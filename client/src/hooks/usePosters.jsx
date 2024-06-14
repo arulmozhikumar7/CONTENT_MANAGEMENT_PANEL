0e4cc0ef9becf093d2d6528bd3367b73dd8a0759
Arulmozhikumar <arulmozhikumar7@gmail.com>
2024-06-14 17:02:11 +0530

    fix: url fixed

--- client/src/hooks/usePosters.jsx
+++ client/src/hooks/usePosters.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const apiUrl = import.meta.env.VITE_API_URL;
 const getPosters = async () => {
   try {
-    const response = await axios.get(apiUrl + `api/poster/`);
+    const response = await axios.get(apiUrl + `/api/poster/`);
     return response.data;
   } catch (error) {
     console.log(error);
@@ -13,7 +13,7 @@ const getPosters = async () => {
 
 const deletePoster = async (id) => {
   try {
-    const response = await axios.delete(apiUrl + `api/poster/${id}`);
+    const response = await axios.delete(apiUrl + `/api/poster/${id}`);
     return response.data;
   } catch (error) {
     console.log(error);
@@ -23,7 +23,7 @@ const deletePoster = async (id) => {
 
 const addPoster = async (data) => {
   try {
-    const response = await axios.post(apiUrl + "api/poster", data);
+    const response = await axios.post(apiUrl + "/api/poster", data);
     return response.data;
   } catch (error) {
     console.log(error);
